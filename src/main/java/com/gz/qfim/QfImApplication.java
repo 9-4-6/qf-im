88dec2738d9dc963685ba0fafe8267cc7c068d25
guozhong <17853522814@163.com>
2025-11-13 23:01:34 +0800

    commit:init

---
+++ src/main/java/com/gz/qfim/QfImApplication.java
@@ -0,0 +1,13 @@
+package com.gz.qfim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QfImApplication.class, args);
+    }
+
+}
