b18248797d3bb057098ee63fa73bf63bb77ec45d
guozhong <17853522814@163.com>
2025-11-15 11:28:42 +0800

    commit: init im-biz project

---
+++ im-biz/src/test/java/org/gz/imbiz/ImBizApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gz.imbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
