242ba4f7e3bfa267c29fc581ad459f6916a062b6
guozhong <17853522814@163.com>
2025-11-28 13:13:46 +0800

    commit: add deadLetterHandler

--- im-biz/src/main/java/org/gz/imbiz/listener/ImMessageConsumer.java
+++ im-biz/src/main/java/org/gz/imbiz/listener/ImMessageConsumer.java
@@ -1,6 +1,7 @@
 package org.gz.imbiz.listener;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Component;
  * @author 17853
  */
 @Component
-@RocketMQMessageListener(consumerGroup = "${rocketmq.consumer.group}", topic = "${rocketmq.topic}")
+@RocketMQMessageListener(consumerGroup = "${rocketmq.consumer.group}",
+        topic = "${rocketmq.consumer.topic}",
+        maxReconsumeTimes = 3,
+        messageModel = MessageModel.CLUSTERING)
 @Slf4j
 public class ImMessageConsumer implements RocketMQListener<String> {
     @Override
