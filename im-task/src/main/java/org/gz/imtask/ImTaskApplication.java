b04a92dea78ceeb3ac0d4a0da1ae3081509a50cc
guozhong <17853522814@163.com>
2025-12-04 15:23:15 +0800

    commit:add im-task

---
+++ im-task/src/main/java/org/gz/imtask/ImTaskApplication.java
@@ -0,0 +1,13 @@
+package org.gz.imtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImTaskApplication.class, args);
+	}
+
+}
